7118d50472b94b16e37b3dd7ab839650e3bd0539
Aleksandar Simic <simic.aleksandar87@gmail.com>
2019-03-13 11:38:55 +0100

    Init project

---
+++ RunningApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunningApp
+//
+//  Created by Aleksandar Simic on 3/13/19.
+//  Copyright © 2019 SimicAleksandar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
